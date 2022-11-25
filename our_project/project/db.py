56a3603787b94b71b3f62511bdccd8b17bda992b
Liu_siri <12094187+liu-siri@user.noreply.gitee.com>
2022-11-25 11:18:24 +0000

    update our_project/project/db.py.
    Signed-off-by: Liu_siri <12094187+liu-siri@user.noreply.gitee.com>

--- our_project/project/db.py
+++ our_project/project/db.py
@@ -1,1 +1,18 @@
 # 数据库功能函数
+import pymysql
+def get_db():
+    #connect database
+    conn = pymysql.connect(
+        host='43.138.62.72',  #IP
+        port=6666,
+        user='root',
+        password='123456',
+        db='six_crew',
+        charset='utf8'
+    )
+    return conn  #establish link
+
+def close_db(conn):
+    # close link
+    conn.close()
+    return
